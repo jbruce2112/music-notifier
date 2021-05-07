b655a85df80afa8c303c1fd9c748cd2eb193ca9a
John Bruce <jbruce2112@gmail.com>
2021-05-06 22:46:00 -0700

    Initial Commit

---
+++ MusicNotifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicNotifier
+//
+//  Created by John on 5/6/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MusicNotifier/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MusicNotifier
+//
+//  Created by John on 5/6/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
